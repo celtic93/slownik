7fd3742747c551753bfa447d90ed8f9b6c185f35
celtic93 <egorka80@list.ru>
2024-05-22 14:02:32 +0300

    Updated Word uniqueness validation

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,7 +2,7 @@ class Word < ApplicationRecord
   has_many :user_words
 
   validates :native, :ru, :pronunciation, :locale, presence: true
-  validates :native, uniqueness: true, if: -> { ru.present? }
+  validates :native, uniqueness: { scope: :locale }, if: -> { ru.present? }
 
   enum locale: {
     native: 0,
