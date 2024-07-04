103c8a0810f267991eaae522c5bdc759beeb9ffc
celtic93 <egorka80@list.ru>
2024-07-04 10:50:43 +0300

    Updated Word creation

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,6 @@
 class WordsController < ApplicationController
+  OPPOSITE_WORD_KIND = "vocabulary".freeze
+
   before_action :set_word, only: %i[show edit update destroy]
 
   def index
@@ -15,10 +17,10 @@ class WordsController < ApplicationController
 
   def create
     @word = Word.new(word_params.merge(locale: :ru))
-    @word2 = Word.new(word_params.merge(locale: :native))
+    @word2 = Word.new(word_params.merge(locale: :native)) if need_to_create_opposite_word
 
-    if @word.save && @word2.save
-      redirect_to words_url, notice: 'Word was successfully created.'
+    if @word.save && (@word2.nil? || @word2.save)
+      redirect_to words_url, notice: "Word was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
@@ -44,6 +46,10 @@ class WordsController < ApplicationController
   end
 
   def word_params
-    params.fetch(:word, {}).permit(:native, :ru, :pronunciation)
+    params.fetch(:word, {}).permit(:native, :ru, :pronunciation, :kind)
+  end
+
+  def need_to_create_opposite_word
+    word_params[:kind] == OPPOSITE_WORD_KIND
   end
 end
